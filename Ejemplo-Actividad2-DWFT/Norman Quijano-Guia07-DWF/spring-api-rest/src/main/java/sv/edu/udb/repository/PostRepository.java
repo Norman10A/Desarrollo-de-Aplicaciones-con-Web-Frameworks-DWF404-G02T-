f79fc3dc8e083b57491eba95f35462acf6201c99
Norman10A <quijanoenmanuel@gmail.com>
2025-10-23 22:03:21 -0600

    Delete Ejemplo-Actividad2-DWFT/Norman Quijano-Guia07-DWF directory

--- Ejemplo-Actividad2-DWFT/Norman Quijano-Guia07-DWF/spring-api-rest/src/main/java/sv/edu/udb/repository/PostRepository.java
+++
@@ -1,5 +0,0 @@
-package sv.edu.udb.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import sv.edu.udb.repository.domain.Post;
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
